08cc9b72f1e0a5dd6c63ac7865ee3022c8f2b554
soniqmidd <soniqmidd@users.noreply.github.com>
2023-11-27 20:11:02 -0600

    Added js for search engine

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Hello</header>
+      <div className="container">
+        <header className="App-header">Hello</header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="text-center">
+          <small>Coded by Sonique Middleton</small>
+        </footer>
+      </div>
     </div>
   );
 }
